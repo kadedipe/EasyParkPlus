63a6110f3f941e00a2d246b6c0cbb5eaf186e1a0
Kolapo Adedipe <kolapoadedipe36@gmail.com>
2026-02-28 14:56:45 +0300

    Updated

---
+++ assets/enums/__init__.py
@@ -0,0 +1,21 @@
+"""Enum exports for the parking management system."""
+
+from parking_management.assets.constants import (
+    ReservationStatus,
+    PaymentStatus,
+    UserRole,
+    UserStatus,
+    ParkingSpotType,
+    VehicleType,
+    NotificationType,
+)
+
+__all__ = [
+    'ReservationStatus',
+    'PaymentStatus',
+    'UserRole',
+    'UserStatus',
+    'ParkingSpotType',
+    'VehicleType',
+    'NotificationType',
+]
